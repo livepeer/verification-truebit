48a6fbd7a9c6de7cbb14d5a5b13d4bb8ba6fc71a
Yondon Fu <yondon.fu@gmail.com>
2018-02-22 13:46:14 -0800

    Upload WASM binary in migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,18 +1,16 @@
 const fs = require("fs")
 const path = require("path")
 const { promisify } = require("util")
+const { createIPFSFile } = require("../client/lib/ipfs")
 
 const TrueBitMock = artifacts.require("TrueBitMock")
 const JobsManager = artifacts.require("JobsManager")
 
-// Note: Placeholder IPFS hash
-const CODE_HASH = "QmXKxSKhUZnmjb53HzS94arpshet3N5Kmct8JBAsgm9umR"
-// Note: Placeholder root hash
-const CODE_ROOT_HASH = web3.sha3("hello")
 // Corresponds to bitrate = 6000k, framerate: 60fps, aspectratio: 16:9, resolution: 1280x720
 const TRANSCODING_OPTIONS = "a7ac137a"
 
 const ENV_FILE = path.resolve(__dirname, "../.env")
+const WASM_FILE = path.resolve(__dirname, "../data/verification.wasm")
 
 const deploy = async (deployer, artifact, ...args) => {
     await deployer.deploy(artifact, ...args)
@@ -28,8 +26,10 @@ const updateEnv = async (account, jobsManagerAddress, trueBitAddress) => {
 
 module.exports = function(deployer, network, accounts) {
     deployer.then(async () => {
+        // Upload global WASM binary for Livepeer protocol
+        const res = await createIPFSFile(WASM_FILE)
         const trueBit = await deploy(deployer, TrueBitMock)
-        const jobsManager = await deploy(deployer, JobsManager, trueBit.address, CODE_HASH, CODE_ROOT_HASH, TRANSCODING_OPTIONS)
+        const jobsManager = await deploy(deployer, JobsManager, trueBit.address, res.ipfsHash, res.root, TRANSCODING_OPTIONS)
 
         await updateEnv(accounts[0], jobsManager.address, trueBit.address)
     })
