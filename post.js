b794c6e5dd319c107e2adce9e8584c4c08aeeedb
Sami Mäkelä <mrsmkl@gmail.com>
2018-04-03 10:05:25 +0300

    separated deployinh and uploading

--- post.js
+++ post.js
@@ -1,16 +1,10 @@
 
-
 var fs = require("fs")
 var Web3 = require('web3')
 var web3 = new Web3()
 var ipfsAPI = require('ipfs-api')
 
-var dir = "./compiled/"
-
-var code = "0x" + fs.readFileSync(dir + "Task.bin")
-var abi = JSON.parse(fs.readFileSync(dir + "Task.abi"))
-
-var config = JSON.parse(fs.readFileSync("../webasm-solidity/node/config.json"))
+var config = JSON.parse(fs.readFileSync("config.json"))
 
 var host = config.host
 
@@ -22,6 +16,7 @@ web3.setProvider(w3provider)
 var ipfs = ipfsAPI(host, '5001', {protocol: 'http'})
 
 var filesystem = new web3.eth.Contract(JSON.parse(fs.readFileSync("../webasm-solidity/contracts/compiled/Filesystem.abi")), config.fs)
+var contract = new web3.eth.Contract(JSON.parse(fs.readFileSync("./compiled/Task.abi")), config.post)
 
 function arrange(arr) {
     var res = []
@@ -52,16 +47,7 @@ async function outputFile(id) {
     // console.log("DEBUG: ", dta)
 }
 
-/*
-function stringToBytes(str) {
-    var lst = []
-    for (var i = 0; i < str.length; i++) {
-        lst.push(str.charCodeAt(i))
-    }
-    return lst
-}*/
-
-var wasm_path = "/home/sami/ocaml-offchain/interpreter/wasm"
+var wasm_path = "../ocaml-offchain/interpreter/wasm"
 
 var execFile = require('child_process').execFile
 
@@ -97,24 +83,11 @@ function stringToBytes(str) {
     return "0x" + lst.toString("hex")
 }
 
-async function checkScrypt(contract, test) {
-    var result = await contract.methods.scrypt(test).call(send_opt)
-    console.log("Testing call", result)
-}
+var fname = process.argv[2] || "input.ts"
 
-async function doDeploy() {
-    var send_opt = {gas:4700000, from:config.base}
-    var info = JSON.parse(fs.readFileSync("info.json"))
-    // var init_hash = "0x0dc2d39106f5f0165019f9acfc0006976928d5c33d5ca0b165a256db30416e39"
-    // var code_address = 'QmToCAmaHkz78LFD4tZV43wNv9FAQRxWCtCSecKvWsJMef'
-    var contract = await new web3.eth.Contract(abi).deploy({data: code, arguments:[config.tasks, config.fs, info.ipfshash, info.codehash]}).send(send_opt)
-    config.post = contract.options.address
-    console.log(JSON.stringify(config))
+async function doPost() {
     
-    /*
-    contract.setProvider(w3provider)
-    
-    var file = await createIPFSFile("input.ts")
+    var file = await createIPFSFile(fname)
     console.log("Uploaded to IPFS", file)
     var file_hash = await contract.methods.submit(file.hash, file.root, file.size).call(send_opt)
     console.log(file_hash)
@@ -131,11 +104,11 @@ async function doDeploy() {
         console.log("Consuming", ev.returnValues)
         process.exit(0)
     })
-    */
-    process.exit(0)
+    // process.exit(0)
 }
 
-doDeploy()
+doPost()
+
 
 
 
